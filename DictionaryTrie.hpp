0aca35d1dbdd9a49fb27e20037f946babc9b574d
Daryl <dnak@no-reply.github.com>
2019-04-29 21:27:30 -0700

    Add dictionary files

--- DictionaryTrie.hpp
+++ DictionaryTrie.hpp
@@ -1,42 +1,237 @@
 /**
- *  CSE 100 PA2 C++ Autocomplete
- *  Authors: Jor-el Briones, Christine Alvarado
+ * Filename:     DictionaryTrie.hpp
+ *
+ * Team:         Brandon Olmos (bolmos@ucsd.edu),
+ *               Daryl Nakamoto (dnakamot@ucsd.edu)
+ *
+ * Reference(s): cplusplus.com
+ *
+ * Description:  Ternery trie to hold strings and their corresponding
+ *               occuring frequency. Suggest n most probable word completions
+ *               based on frequency of words with a particular prefix.
  */
 
-#ifndef DICTIONARY_TRIE_HPP
-#define DICTIONARY_TRIE_HPP
+#ifndef DICTIONARYTRIE_HPP
+#define DICTIONARYTRIE_HPP
 
 #include <vector>
 #include <string>
 #include <queue>
+#include "TNode.hpp"
+
+#define EMPTYSTR ""
+typedef pair<string, int> Word;
+
 using namespace std;
 /**
- *  The class for a dictionary ADT, implemented as a trie
- *  You may implement this class as either a mulit-way trie
- *  or a ternary search trie, but you must use one or the other.
+ *  The class for a dictionary ADT, implemented as a ternery trie
  */
+
+
 class DictionaryTrie
 {
+private:
+
+    /** Comparator struct for TNodes. (priority queue)*/
+    struct Compare {
+    public:
+        bool operator()(const Word& str1, const Word& str2)
+        {
+            if(str1.second == str2.second)
+                return str1.first > str2.first;
+
+            return str1.second < str2.second;
+        }
+    };
+
+    priority_queue<Word, vector<Word>, Compare> numComplete;
+    TNode* root;
+
+    /** predictCompletions helper function; return ALL suggestions */
+    void getCompletions(string word, TNode* curr) {
+
+        // naive post-order traversal to get all words for a given prefix
+
+        if(curr == nullptr) return;
+
+        getCompletions(word, curr->left);
+
+        getCompletions(word, curr->right);
+
+        if(curr->middle) getCompletions(word + curr->_char, curr->middle);
+
+
+        // insert if a word
+        if(curr->freq > 0)
+            numComplete.push(Word(word + curr->_char, curr->freq));
+
+    }
+
 public:
 
-  /* Create a new Dictionary that uses a Trie back end */
-  DictionaryTrie();
+  /** Create a new Dictionary that uses a Trie back end */
+  DictionaryTrie() : root(nullptr) {}
 
-  /** 
-   * Insert a word with its frequency into the dictionary.
-   * Return true if the word was inserted, and false if it
-   * was not (i.e. it was already in the dictionary or it was
-   * invalid (empty string). This might be useful for testing
-   * when you want to test a certain case, but don't want to
-   * write out a specific word 300 times.
+/**
+ * Insert a word with its frequency into the dictionary.
+ * Return true if the word was inserted, and false if it
+ * was not (i.e. it was already in the dictionary or it was
+ * invalid (empty string). This might be useful for testing
+ * when you want to test a certain case, but don't want to
+ * write out a specific word 300 times.
+ */
+    bool insert(string word, unsigned int freq)
+    {
+
+        // reject empty string
+        if(word == EMPTYSTR) return false;
+
+        TNode* curr;
+        int index = 0;
+        int length = word.length();
+
+        // single character word AND initial build (empty trie)
+        if(root == nullptr) {
+
+            root = new TNode(word[index]);
+
+            if(length == 1) {
+
+                root->freq = freq;
+                return true;
+            }
+        }
+
+        curr = root;
+
+        // start insertion
+        while(index < length) {
+
+            // check middle child
+            if(curr->_char == word[index])
+            {
+
+                if(curr->middle == nullptr)
+                {
+                    // reject duplicate (no middle child)
+                    if(index + 1 == length) return false;
+
+                    else
+                    {
+                        curr->middle = new TNode(word[index + 1]);
+
+                        // done inserting word
+                        if(index + 1 == length - 1)
+                        {
+                            if(curr->fmid < freq) curr->fmid = freq;
+                            curr->middle->freq = freq;
+                            return true;
+                        }
+                    }
+                }
+
+                // check next character in string
+                if(curr->fmid < freq) curr->fmid = freq;
+                curr = curr->middle;
+                index++;
+            }
+
+            // check left child
+            else if(word[index] < curr->_char)
+            {
+
+                if(curr->left == nullptr)
+                {
+                    // create new left node if necessary
+                    curr->left = new TNode(word[index]);
+
+                    if (index + 1 == length) {
+                        if(curr->fleft < freq) curr->fleft = freq;
+                        curr->left->freq = freq;
+                        return true;
+                    }
+                }
+
+                if(curr->fleft < freq) curr->fleft = freq;
+                curr = curr->left;
+            }
+
+            // check right child
+            else
+            {
+
+                if(curr->right == nullptr)
+                {
+                    curr->right = new TNode(word[index]);
+
+                    if(index + 1 == length)
+                    {
+                        if(curr->fright < freq) curr->fright = freq;
+                        curr->right->freq = freq;
+                        return true;
+                    }
+                }
+
+                if(curr->fright < freq) curr->fright = freq;
+                curr = curr->right;
+            }
+
+        }
+
+        // reject duplicate
+        return false;
+    }
+
+  /** Return true if word is in the dictionary, and false otherwise.
    */
-  bool insert(std::string word, unsigned int freq);
+  bool find(string word) const
+  {
 
-  /* Return true if word is in the dictionary, and false otherwise. */
-  bool find(std::string word) const;
+      if(word == EMPTYSTR || root == nullptr) return false;
 
-  /* 
-   * Return up to num_completions of the most frequent completions
+      TNode* curr = root;
+      int index = 0;
+      int length = word.length();
+
+      // simply, perform trinary search
+      while(index < length)
+      {
+          // check middle and check if word
+          if(curr->_char == word[index])
+          {
+
+              index++;
+
+              if(index == length && curr->freq > 0) return true;
+
+              else if(curr->middle == nullptr) return false;
+
+              else curr = curr->middle;
+          }
+
+          // check left
+          else if(word[index] < curr->_char)
+          {
+              if(curr->left == nullptr) return false;
+
+              else curr = curr->left;
+          }
+
+
+          // check right
+          else
+          {
+              if(curr->right == nullptr) return false;
+
+              else curr = curr->right;
+          }
+      }
+
+      return false;
+  }
+
+
+  /** Return up to num_completions of the most frequent completions
    * of the prefix, such that the completions are words in the dictionary.
    * These completions should be listed from most frequent to least.
    * If there are fewer than num_completions legal completions, this
@@ -46,10 +241,62 @@ public:
    * is a word (and is among the num_completions most frequent completions
    * of the prefix)
    */
-  std::vector<std::string>
-  predictCompletions(std::string prefix, unsigned int num_completions);
+  vector<string> predictCompletions(string prefix, unsigned int num_completions)
+  {
+      numComplete = priority_queue<Word, vector<Word>, Compare>();
+      vector<string> mostFreqStr = vector<string>();
 
-  /* Return up to num_completions of the most frequent completions
+      TNode* curr = root;
+      int index = 0;
+      int preLength = prefix.length();
+
+      // no completion suggestions
+      if(num_completions == 0) return  mostFreqStr;
+
+      // go to prefix position
+      while(curr && index < preLength)
+      {
+          if(curr->_char == prefix[index])
+          {
+              index++;
+
+              if(index != preLength) curr = curr->middle;
+          }
+
+          else if(prefix[index] < curr->_char)
+          {
+              curr = curr->left;
+          }
+
+          else
+          {
+              curr = curr->right;
+          }
+      }
+
+      // prefix not found
+      if(curr == nullptr) return mostFreqStr;
+
+      // if prefix is a word, add to queue
+      if(curr->freq > 0)
+          numComplete.push(Word(prefix, curr->freq));
+
+      // start autocompletion
+      if(curr->middle)
+        getCompletions(prefix, curr->middle);
+
+      unsigned int i = 0;
+      while(numComplete.size() != 0 && i < num_completions) {
+          mostFreqStr.push_back(numComplete.top().first);
+          numComplete.pop();
+          ++i;
+      }
+
+      return mostFreqStr;
+
+  }
+
+  /**Return up to num_completions of the most frequent completions
    * of the pattern, such that the completions are words in the dictionary.
    * These completions should be listed from most frequent to least.
    * If there are fewer than num_completions legal completions, this
@@ -59,14 +306,29 @@ public:
    * is a word (and is among the num_completions most frequent completions
    * of the pattern)
    */
-  std::vector<std::string>
-  predictUnderscore(std::string pattern, unsigned int num_completions);
+  vector<string> predictUnderscore(string pattern, unsigned int num_completions){
+      vector<string> empty;
 
-  /* Destructor */
-  ~DictionaryTrie();
+      return empty;
+  }
 
-private:
-  // Add your own data members and methods here
+  /** Destructor */
+  ~DictionaryTrie() {
+     deleteTree(root);
+  }
+
+  /** post-order traversal to delete trie */
+  void deleteTree(TNode* curr) {
+      if(curr == nullptr) return;
+
+      if(curr->left) deleteTree(curr->left);
+
+      if(curr->middle) deleteTree(curr->middle);
+
+      if(curr->right) deleteTree(curr->right);
+
+      delete curr;
+  }
 };
 
-#endif // DICTIONARY_TRIE_H
+#endif // DICTIONARYTRIE_HPP
--- autocomplete.cpp
+++ autocomplete.cpp
@@ -1,5 +1,21 @@
+/**
+ * Filename:     autocomplete.cpp
+ *
+ * Team:         Brandon Olmos (bolmos@ucsd.edu),
+ *               Daryl Nakamoto (dnakamot@ucsd.edu)
+ *
+ * Reference(s): cplusplus.com
+ *
+ * Description:  Read a dictionary into ternary trie and suggest
+ *               autocompletion K results to users based on an input prefx
+ *               and K number of wanted suggested completions.
+ */
+
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "DictionaryTrie.hpp"
+#include "util.hpp"
 
 using namespace std;
 
@@ -17,42 +33,86 @@ using namespace std;
  */
 int main(int argc, char** argv)
 {
-    // incorrect argument
+    /** DEBUGGING */
+
+    string test = "hello";
+    string hold;
+
+    while(test.length() > 0)
+    {
+        cout << test << endl;
+        test.erase(0, 1);
+    }
+
+    DictionaryTrie trie;
+    DictionaryTrie trie2;
+
+    bool check1 = false;
+
+
+    check1 = trie.insert("g", 10);
+    check1 = trie.insert("a", 1);
+    check1 = trie.insert("h", 1000);
+    check1 = trie.insert("g", 10);
+    check1 = trie.insert("a", 1);
+    check1 = trie.insert("h", 1000);
+
+    check1 = trie2.insert("get", 30000);
+    check1 = trie2.insert("apple", 1);
+    check1 = trie2.insert("gat", 2);
+    check1 = trie2.insert("git", 3);
+    check1 = trie2.insert("git", 3);
+    check1 = trie2.insert("github", 1337);
+
+    //*/
+
+    // exit  due to incorrect argument count
     if(argc != 2) {
         cout << "This program needs exactly one argument!" << endl;
         return -1;
     }
 
-    string file = argv[1];
-    string prefix;
-    string check;
-    int numCompletions;
+    string file = argv[1];          // get file name
+    string prefix;                  // hold prefix
+    string getCompletions;          // hold number of completions
+    unsigned int numCompletions;    //
     char _continue;
+    Utils read;
+    DictionaryTrie dictionary;
+    vector<string> completions;
+    ifstream load;
 
     cout << "Reading file: " << file << endl;
-    /**
-     * READ FILE
-     */
 
+    load.open(file, ifstream::in);
+
+    read.load_dict(dictionary, load);
+
+    load.close();
+
+
+    // start program
     do {
         cout << "Enter a prefix/pattern to search for:" << endl;
-        cin >> prefix;
-
+        getline(cin, prefix);
 
         cout << "Enter a number of completions:" << endl;
-        cin >> check;
+        cin >> getCompletions;
 
+        istringstream convert(getCompletions);
 
-        // DEBUGGING - check input values
-//    cout << prefix << endl;
-//    cout << numCompletions << endl;
+        convert >> numCompletions;
 
-        /**
-         * DO AUTOCOMPLETION
-         */
+        completions = dictionary.predictCompletions(prefix, numCompletions);
+
+        for(auto itr = completions.begin(); itr != completions.end(); ++itr)
+            cout << *itr << endl;
 
         cout << "Continue? (y/n)" << endl;
         cin >> _continue;
+
+        cin.ignore();
+
     } while(_continue == 'y');
 
     return 0;
